afb334ded3bb98805d440fcb2ccb7ae9ad8498b1
Christopher Melby <melby.christopher@gmail.com>
2019-12-10 15:43:17 -0800

    html formmating set up, js file started and linking to api complete for current weather.

--- script.js
+++ script.js
@@ -1,2 +1,29 @@
 
 
+//define the varibles that we'll need to append data pulled from api to cards above---------------------------------//
+var APIkey = "&appid=ecc0be5fd92206da3aa90cc41c13ca56";
+var cityName = "Seattle"
+var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=Seattle&appid=ecc0be5fd92206da3aa90cc41c13ca56";
+
+
+
+//----------------------Functions for current weather by city---------------------//
+function displayWeather () {
+    
+    var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + cityName + APIkey;
+
+    $.ajax({
+    url: queryURL,
+    method: "GET"
+    }).then(function(response) {
+    console.log(response);
+    });
+
+}
+displayWeather();
+
+//-----------------------Function for 5 day forcast by city-----------------//
+
+
+
+//------------------------Function for search history-----------------------//
