593b7edd79131185ef6a1748240f3561e178071d
Christopher Melby <melby.christopher@gmail.com>
2019-12-10 22:45:01 -0800

    event listener complete, search function complete, and search term stores locally. Still need: to get the stored term and dynamically render them on the page as well as pull in the 5 day forcast which I think will require another api link, same with the UV info. I also need to make the search link the entire api and not just Seattle by changing the syntax of the var query url now that I have it working.

--- script.js
+++ script.js
@@ -1,29 +1,77 @@
 
 
-//define the varibles that we'll need to append data pulled from api to cards above---------------------------------//
-var APIkey = "&appid=ecc0be5fd92206da3aa90cc41c13ca56";
-var cityName = "Seattle"
+// //define the varibles that we'll need to append data pulled from api to cards above---------------------------------//
+// var APIkey = "&appid=ecc0be5fd92206da3aa90cc41c13ca56";
+// var cityName = "Seattle"
+// var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=Seattle&appid=ecc0be5fd92206da3aa90cc41c13ca56";
+
+
+//-----------------------Search Function for Current City Weather-----------------//
+
+function searchCity(cityname) {
+
 var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=Seattle&appid=ecc0be5fd92206da3aa90cc41c13ca56";
 
-
-
-//----------------------Functions for current weather by city---------------------//
-function displayWeather () {
-    
-    var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + cityName + APIkey;
-
-    $.ajax({
+$.ajax({
     url: queryURL,
-    method: "GET"
-    }).then(function(response) {
+    method: 'GET'
+}).then(function(response) {
     console.log(response);
-    });
+    console.log(queryURL);
+    //empty divs and ids that we need to dump content into
+    $("#current").empty();
+
+    // $('#UV').empty();
+    
+    //create HTML for city information......
+    var cityNameEl = $("<h2>").text(response.name);
+    var tempEL = $("<p>").text(response.main.temp);
+    var humEl = $("<p>").text(response.main.humidity);
+    //create HTML div to append new elements to render on page....
+    var newDiv = $('<div>');
+
+    newDiv.append(cityNameEl, tempEL, humEl);
+
+    $("#current").html(newDiv);
+
+});
+
+
 
 }
-displayWeather();
-
-//-----------------------Function for 5 day forcast by city-----------------//
 
 
+//------------------------Event handler for user city search-----------------------//
 
-//------------------------Function for search history-----------------------//
+
+$("#select-city").on("click", function(event) {
+// Preventing the button from trying to submit the form
+    event.preventDefault();
+// Storing the city name
+    var cityInput = $("#city-input").val().trim();
+
+//save search term to local storage.....
+ var textContent = $(this)
+ .siblings("input")
+ .val();
+localStorage.setItem(cityInput, textContent);
+
+ // Running the searchCity function (passing in the city as an argument) 
+ searchCity(cityInput);
+});
+console.log(event);
+
+
+
+//current --
+// name date icon
+// Temp
+// humidity 
+//wind speed
+// UV
+
+//5 day ---
+//date:
+//icon
+//Temp:
+//humidity:
